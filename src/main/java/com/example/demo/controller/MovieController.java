e9cc2c0e9d52a7e6e8c3d3ad30d4b5e0a4fdb764
Alonso <alo.gomezc@gmail.com>
2024-08-31 22:58:37 -0400

    feat: Incluir a todos ResponseEntity en Controller

--- src/main/java/com/example/demo/controller/MovieController.java
+++ src/main/java/com/example/demo/controller/MovieController.java
@@ -1,11 +1,13 @@
 package com.example.demo.controller;
 
+import com.example.demo.exception.ObjectNotFoundException;
 import com.example.demo.persistence.entity.Movie;
 import com.example.demo.service.MovieService;
 import com.example.demo.service.impl.MovieServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,7 +19,34 @@ public class MovieController {
     @Autowired
     private MovieService movieService;
 
-    
+    @GetMapping("/movie")
+    public ResponseEntity<?> findMovies(@RequestParam (required = false) String title){
+        List<Movie> movies = null;
+
+        if (StringUtils.hasText(title) && title!=null){
+            System.out.println("PASO POR FINDBYTITLE");
+            movies = movieService.findByTitle(title);
+
+        } else {
+            movies = movieService.findAll();
+            System.out.println("PASO POR FINDALL");
+
+        }
+        return ResponseEntity.ok(movies);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
+    ResponseEntity<Movie> findOneById(@PathVariable Long id){
+
+        try {
+            Movie movie = movieService.findOneById(id);
+            return new ResponseEntity(movie,HttpStatus.OK);
+        }catch (ObjectNotFoundException exception) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+
+    }
 
     @PostMapping("/post")
     public  ResponseEntity<?> createMovie(@RequestBody Movie movie){
--- src/main/java/com/example/demo/controller/UserAccountController.java
+++ src/main/java/com/example/demo/controller/UserAccountController.java
@@ -6,10 +6,12 @@ import com.example.demo.service.impl.UserAccountServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -22,6 +24,12 @@ public class UserAccountController {
     public ResponseEntity<?> findAll(){
         List<UserAccount> users = userAccountService.findAll();
         return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<?> findOneByUsername(@PathVariable String username){
+        UserAccount user = userAccountService.findOneByUsername(username);
+        return ResponseEntity.ok(user);
     }
 
     
